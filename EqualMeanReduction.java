1f507d632def27e82d3d2428dd21bc8e212a1bb5
boorlaraju <boorlaraju404@gmail.com>
2025-03-11 14:26:24 +0530

    java program

---
+++ EqualMeanReduction.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class EqualMeanReduction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();  // Number of test cases
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();  // Length of array
+            int x = sc.nextInt();  // Target value
+            
+            int[] a = new int[n];
+            int sum = 0;
+            
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+                sum += a[i];
+            }
+            
+            // If the mean of the array equals x, it's possible
+            if (sum == x * n) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        
+        sc.close();
+    }
+}
